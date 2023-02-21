041216736cb786e2024954e2b7c87e78558c6bff
code <ashish@tryroll.com>
2023-02-21 09:33:23 +0530

    update funciton names, added values nad vars defining ways

---
+++ javascript/values_and_variables/script.js
@@ -0,0 +1,17 @@
+let firstName = "test"
+console.log(firstName) // log in console
+
+// reserve keywords 
+/*
+ new
+ function
+*/
+
+// if want to use reserver keyword we can do something like 
+let $funcion = "function reserve keywork usage"
+console.log($funcion)
+
+// for cleaner code variable names should be descriptive
+// like 
+let myFirstName = "myFirstName"
+let myLastName = "myLastName"
