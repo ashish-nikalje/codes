7353cf6dc6307fc9c245c1e3fcc24476848383a2
code <ashish@tryroll.com>
2023-02-21 09:13:27 +0530

    create and link javascript file in html file
    moved to folder

---
+++ javascript/Assignment-1/README.md
@@ -0,0 +1,3 @@
+# Assignment 1
+
+### In this we are going to create sample javascript and going to link that file with html file and run it on browser
---
+++ javascript/Assignment-1/script.js
@@ -0,0 +1,12 @@
+let js = 'testing';
+if (js == 'testing') {
+    alert("condition satisfied..!")
+} else if (js == "not testing") {
+    alert("condition not satisfied...!")
+} // normal if else case
+
+//calculation print in console
+console.log("addition: ", 40 + 80)
+console.log("substraction: ", 40 - 80)
+console.log("division: ", 40 / 80)
+console.log("multiply: ", 40 * 80)
